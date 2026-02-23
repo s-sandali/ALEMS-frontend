057013fc36800c5e963c62e6a6b309f1931af8ba
Sandali.js <sandalisandagomi@gmail.com>
2026-02-23 22:11:22 +0530

    added react router nesting for the loging dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import { Routes, Route } from "react-router-dom";
+import {
+    SignIn,
+    SignUp,
+    SignedIn,
+    SignedOut,
+    RedirectToSignIn,
+} from "@clerk/clerk-react";
 import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
     return (
         <div className="min-h-screen" style={{ fontFamily: "'Poppins', sans-serif" }}>
-            <LandingPage />
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+
+                <Route
+                    path="/login/*"
+                    element={<SignIn routing="path" path="/login" />}
+                />
+
+                <Route
+                    path="/register/*"
+                    element={<SignUp routing="path" path="/register" />}
+                />
+
+                <Route
+                    path="/dashboard"
+                    element={
+                        <>
+                            <SignedIn>
+                                <Dashboard />
+                            </SignedIn>
+                            <SignedOut>
+                                <RedirectToSignIn />
+                            </SignedOut>
+                        </>
+                    }
+                />
+            </Routes>
         </div>
     );
 }
